73af5ca226eb3cf3b24e3eb0276a5ae382ac2e0d
izijoe <jospaco2001@yahoo.com>
2022-11-19 10:38:53 -0800

    initial commit

--- utils.c
+++ utils.c
@@ -62,7 +62,7 @@ long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
-	alse if (size == S_SHORT)
+	else if (size == S_SHORT)
 		return ((short)num);
 
 	return ((int)num);
